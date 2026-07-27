690b8eefbfd585eff96a73ccd67c0534fbef4df2
zesciati <zakyabiyyu2003@gmail.com>
2026-07-27 21:51:36 +0700

    mobile design done

--- src/App.vue
+++ src/App.vue
@@ -6,17 +6,65 @@
   >
     <section class="flex justify-between bg-red-500 w-full p-5 rounded-md">
       <div class="text-white">
-        <p class="">My balance</p>
-        <p class="text-2xl">$921.48</p>
+        <p class="text-lg">My balance</p>
+        <p class="text-3xl font-bold">$921.48</p>
       </div>
       <img src="/images/logo.svg" alt="" />
     </section>
 
-    <section class="w-full p-5 rounded-md bg-white">
+    <section class="w-full p-5 rounded-md bg-white space-y-7">
       <h2 class="text-2xl font-semibold">Spending - Last 7 days</h2>
-      
+      <article class="flex gap-2 flex-wrap pt-10">
+        <div class="space-y-2">
+          <div class="bg-red-500 h-30 w-10 rounded-sm"></div>
+          <p class="text-center text-brown-400">mon</p>
+        </div>
+
+        <div class="space-y-2">
+          <div class="bg-red-500 h-30 w-10 rounded-sm"></div>
+          <p class="text-center text-brown-400">tue</p>
+        </div>
+        <div class="space-y-2">
+          <div class="bg-red-500 h-30 w-10 rounded-sm"></div>
+          <p class="text-center text-brown-400">wed</p>
+        </div>
+        <div class="space-y-2">
+          <div class="bg-red-500 h-30 w-10 rounded-sm"></div>
+          <p class="text-center text-brown-400 ">thu</p>
+        </div>
+
+        <div class="space-y-2">
+          <div class="bg-red-500 h-30 w-10 rounded-sm"></div>
+          <p class="text-center text-brown-400">fri</p>
+        </div>
+
+        <div class="space-y-2">
+          <div class="bg-red-500 h-30 w-10 rounded-sm"></div>
+          <p class="text-center text-brown-400">sat</p>
+        </div>
+
+        <div class="space-y-2">
+          <div class="bg-red-500 h-30 w-10 rounded-sm"></div>
+          <p class="text-center text-brown-400">sun</p>
+        </div>
+      </article>
+
+      <div class="border border-red-100"></div>
+
+      <article class="flex justify-between items-center">
+        <div>
+          <p class="text-lg text-brown-400">Total this month</p>
+          <h2 class="text-4xl font-semibold">$478.33</h2>
+        </div>
+
+        <div class="text-right">
+          <p class="font-semibold text-lg">+2.4%</p>
+          <p class="text-lg text-brown-400">from last month</p>
+        </div>
+      </article>
     </section>
   </main>
 </template>
 
 <style scoped></style>
+
