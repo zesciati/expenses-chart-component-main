dfebe7dd7d2c103c0af6cf577416232522c8a224
zesciati <zakyabiyyu2003@gmail.com>
2026-07-28 09:43:56 +0700

    adding pc design

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,9 @@
 
 <template>
   <main
-    class="flex justify-center items-center h-screen flex-col bg-red-100 px-5 space-y-4"
+    class="flex justify-center items-center h-screen flex-col bg-red-100 px-5 space-y-4 "
   >
-    <section class="flex justify-between bg-red-500 w-full p-5 rounded-md">
+    <section class="flex justify-between bg-red-500 w-full p-5 rounded-md sm:w-120 sm:max-w-120">
       <div class="text-white">
         <p class="text-lg">My balance</p>
         <p class="text-3xl font-bold">$921.48</p>
@@ -12,39 +12,39 @@
       <img src="/images/logo.svg" alt="" />
     </section>
 
-    <section class="w-full p-5 rounded-md bg-white space-y-7">
+    <section class="w-full p-5 rounded-md bg-white space-y-7 sm:w-120 max-w-120">
       <h2 class="text-2xl font-semibold">Spending - Last 7 days</h2>
-      <article class="flex gap-2 flex-wrap pt-10">
+      <article class="flex gap-2 flex-wrap pt-10 sm:gap-4 mx-auto">
         <div class="space-y-2">
-          <div class="bg-red-500 h-30 w-10 rounded-sm"></div>
+          <div class="bg-red-500 h-30 w-10 rounded-sm sm:w-12"></div>
           <p class="text-center text-brown-400">mon</p>
         </div>
 
         <div class="space-y-2">
-          <div class="bg-red-500 h-30 w-10 rounded-sm"></div>
+          <div class="bg-red-500 h-30 w-10 rounded-sm sm:w-12"></div>
           <p class="text-center text-brown-400">tue</p>
         </div>
         <div class="space-y-2">
-          <div class="bg-red-500 h-30 w-10 rounded-sm"></div>
+          <div class="bg-red-500 h-30 w-10 rounded-sm sm:w-12"></div>
           <p class="text-center text-brown-400">wed</p>
         </div>
         <div class="space-y-2">
-          <div class="bg-red-500 h-30 w-10 rounded-sm"></div>
+          <div class="bg-red-500 h-30 w-10 rounded-sm sm:w-12"></div>
           <p class="text-center text-brown-400 ">thu</p>
         </div>
 
         <div class="space-y-2">
-          <div class="bg-red-500 h-30 w-10 rounded-sm"></div>
+          <div class="bg-red-500 h-30 w-10 rounded-sm sm:w-12"></div>
           <p class="text-center text-brown-400">fri</p>
         </div>
 
         <div class="space-y-2">
-          <div class="bg-red-500 h-30 w-10 rounded-sm"></div>
+          <div class="bg-red-500 h-30 w-10 rounded-sm sm:w-12"></div>
           <p class="text-center text-brown-400">sat</p>
         </div>
 
         <div class="space-y-2">
-          <div class="bg-red-500 h-30 w-10 rounded-sm"></div>
+          <div class="bg-red-500 h-30 w-10 rounded-sm sm:w-12"></div>
           <p class="text-center text-brown-400">sun</p>
         </div>
       </article>
